7fe59f254ccc16f8107a04c9d38f91d8579cad3b
camero2734 <ckhaley21@gmail.com>
2023-12-31 21:23:24 +0100

    Add cloudflare worker stuff

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
 console.log("Hello via Bun!");
+
+export default {
+  async fetch(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+
+    return Response.json({ message: `Hello ${navigator.userAgent} at path ${url.pathname}!` });
+  },
+} satisfies ExportedHandler;
