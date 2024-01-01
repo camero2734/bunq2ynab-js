6d607db8613f4a9b78c8f2c857dfc39a1ceebba0
camero2734 <ckhaley21@gmail.com>
2024-01-01 19:39:19 +0100

    Sync bunq <> ynab :)

--- index.ts
+++ index.ts
@@ -1,36 +1,54 @@
-import { MonetaryAccountBank, MonetaryAccountListing } from "./src/bunq/Api";
-import { BunqApi, createClient } from "./src/bunq/client";
+import { MonetaryAccountBank } from "./src/bunq/Api";
+import { createClient } from "./src/bunq/client";
+
+import * as YNAB from 'ynab';
+import { matchedAccounts, occurenceCount, syncTransactionsForAccount, ynabAccountsByIban } from "./src/sync";
+
 interface EnvironmentVars {
   BUNQ_API_KEY: string;
+  YNAB_API_KEY: string;
 }
 
 export default {
   async fetch(request: Request, env: EnvironmentVars): Promise<Response> {
     try {
-      const bc = await createClient(env.BUNQ_API_KEY);
+      matchedAccounts.clear();
+      occurenceCount.clear();
+      ynabAccountsByIban.clear();
+
+      const bunq = await createClient(env.BUNQ_API_KEY);
+      const ynab = new YNAB.API(env.YNAB_API_KEY);
+      const budget = (await ynab.budgets.getBudgets()).data.budgets[0];
 
       // Get all accounts
-      const { data: accountsData } = await bc.user.listAllMonetaryAccountForUser(bc.userId);
+      const { data: accountsData } = await bunq.user.listAllMonetaryAccountForUser(bunq.userId);
       const accounts = accountsData.flatMap(acts => Object.values(acts) as Required<MonetaryAccountBank>[]);
 
+      // Match YNAB/Bunq accounts
+      const ynabAccounts = await ynab.accounts.getAccounts(budget.id);
+      for (const bunqAccount of accounts) {
+        for (const ynabAccount of ynabAccounts.data.accounts) {
+          if (ynabAccount.name === bunqAccount.description) {
+            const iban = bunqAccount.alias.find(alias => alias.type === 'IBAN')?.value;
+            if (!iban) throw new Error(`Could not find IBAN for ${bunqAccount.description}`);
+            ynabAccountsByIban.set(iban, ynabAccount);
+          }
+        }
+      }
+
       // Get all transactions
-      for (const account of accounts) {
-        if (account.status !== 'ACTIVE') continue;
-        console.log(`Account ${account.description}`);
-        await syncTransactionsForAccount(bc, account);
+      for (const bunqAccount of accounts) {
+        if (bunqAccount.status !== 'ACTIVE') continue;
+        console.log(`bunqAccount ${bunqAccount.description}`);
+        await syncTransactionsForAccount({ bunq, ynab, bunqAccount, ynabBudget: budget });
       }
 
       return Response.json({ message: 'ok!' });
     } catch (e) {
       if (e instanceof Response && !e.bodyUsed) return e;
+      console.log(e, /ERROR/);
       throw e;
     }
   },
 } satisfies ExportedHandler<EnvironmentVars>;
 
-async function syncTransactionsForAccount(bc: BunqApi, account: Required<MonetaryAccountBank>) {
-  const transactions = await bc.user.listAllPaymentForUserMonetaryAccount(bc.userId, account.id, { query: { count: 200 } } as any);
-  const mappedTxs = transactions.data.map(tx => tx.Payment);
-
-  console.log(`${mappedTxs.length} transactions`);
-}
---
+++ src/sync.ts
@@ -0,0 +1,64 @@
+import * as YNAB from 'ynab';
+import { BunqApi } from './bunq/client';
+import { MonetaryAccountBank, PaymentListing } from './bunq/Api';
+
+interface SyncParams {
+  bunq: BunqApi;
+  ynab: YNAB.API;
+  bunqAccount: Required<MonetaryAccountBank>;
+  ynabBudget: YNAB.BudgetSummary;
+}
+
+export const matchedAccounts = new Map<number, YNAB.Account>();
+export const occurenceCount = new Map<string, number>();
+export const ynabAccountsByIban = new Map<string, YNAB.Account>();
+
+export async function syncTransactionsForAccount(params: SyncParams) {
+  const { bunq, ynab, bunqAccount, ynabBudget } = params;
+
+  const ynabAccount = ynabAccountsByIban.get(bunqAccount.alias.find(alias => alias.type === 'IBAN')?.value!);
+  if (!ynabAccount) throw new Error(`Could not find YNAB account for ${bunqAccount.description}`);
+
+  const transactions = await bunq.user.listAllPaymentForUserMonetaryAccount(bunq.userId, bunqAccount.id, { query: { count: 200 } } as any);
+  const mappedTxs = transactions.data
+    .sort((a, b) => a.Payment.created!.localeCompare(b.Payment.created!))
+    .map(tx => mapBunqToYnabTransaction(tx.Payment, ynabAccount));
+
+  const saveResponse = await ynab.transactions.createTransactions(ynabBudget.id, { transactions: mappedTxs });
+
+  console.log(`Imported ${mappedTxs.length} transactions (${saveResponse.data.duplicate_import_ids?.length} duplicates)`);
+}
+
+function mapBunqToYnabTransaction(tx: PaymentListing, ynabAccount: YNAB.Account): YNAB.SaveTransaction {
+  if (!tx.amount?.value) throw new Error(`Transaction ${tx.id} has no amount`);
+  if (!tx.created) throw new Error(`Transaction ${tx.id} has no created date`);
+  if (!tx.counterparty_alias) throw new Error(`Transaction ${tx.id} has no counterparty_alias`);
+
+  const amount = Math.round(+tx.amount.value * 1000); // YNAB uses milliunits
+  const date = new Date(tx.created).toISOString().slice(0, 10);
+  const payeeName = tx.counterparty_alias.display_name;
+
+  let payeeId: string | undefined;
+  if (tx.counterparty_alias.iban) {
+    const counterpartyIBAN = tx.counterparty_alias.iban;
+    payeeId = ynabAccountsByIban.get(counterpartyIBAN)?.transfer_payee_id || undefined;
+  }
+
+  const partialImportId = `YNAB:${amount}:${date}`;
+
+  const occurrence = (occurenceCount.get(partialImportId) || 0) + 1;
+  occurenceCount.set(partialImportId, occurrence);
+
+  const importId = `${partialImportId}:${occurrence}`;
+
+  return {
+    account_id: ynabAccount.id,
+    amount,
+    date,
+    payee_name: payeeName,
+    payee_id: payeeId,
+    memo: tx.description?.slice(0, 100),
+    cleared: 'cleared',
+    import_id: importId,
+  };
+}
