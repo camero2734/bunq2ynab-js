e9afac56fd44293df9d47a8ac1a2130ac8387b25
camero2734 <ckhaley21@gmail.com>
2024-01-02 15:39:08 +0100

    Some clean up + run less often

--- index.ts
+++ index.ts
@@ -20,30 +20,34 @@ export default {
 
       const bunq = await createClient(env.BUNQ_API_KEY, env.bunq2ynab, env.ENV === 'development');
       const ynab = new YNAB.API(env.YNAB_API_KEY);
-      const budget = (await ynab.budgets.getBudgets()).data.budgets[0];
 
-      // Get all accounts
+      // Get all bunq accounts
       const { data: accountsData } = await bunq.user.listAllMonetaryAccountForUser(bunq.userId);
       const accounts = accountsData.flatMap(acts => Object.values(acts) as Required<MonetaryAccountBank>[]);
 
+      // Get all YNAB accounts
+      const budget = (await ynab.budgets.getBudgets()).data.budgets[0];
+      const ynabAccounts = (await ynab.accounts.getAccounts(budget.id)).data.accounts;
+
       // Match YNAB/Bunq accounts
-      const ynabAccounts = await ynab.accounts.getAccounts(budget.id);
-      for (const bunqAccount of accounts) {
-        for (const ynabAccount of ynabAccounts.data.accounts) {
-          if (bunqAccount.status !== 'ACTIVE') continue;
-
-          if (ynabAccount.name === bunqAccount.description) {
-            const iban = bunqAccount.alias.find(alias => alias.type === 'IBAN')?.value;
-            if (!iban) throw new Error(`Could not find IBAN for ${bunqAccount.description}`);
-            ynabAccountsByIban.set(iban, ynabAccount);
-          }
-        }
-      }
-
-      // Get all transactions
       for (const bunqAccount of accounts) {
         if (bunqAccount.status !== 'ACTIVE') continue;
-        console.log(`bunqAccount ${bunqAccount.description}`);
+
+        const ynabAccount = ynabAccounts.find(acc => acc.name === bunqAccount.description);
+        if (!ynabAccount) {
+          console.log(`Could not find YNAB account for ${bunqAccount.description}`);
+          continue;
+        }
+
+        const iban = bunqAccount.alias.find(alias => alias.type === 'IBAN')?.value;
+        if (!iban) throw new Error(`Could not find IBAN for ${bunqAccount.description}`);
+        ynabAccountsByIban.set(iban, ynabAccount);
+      }
+
+      // Sync new transactions
+      for (const bunqAccount of accounts) {
+        if (bunqAccount.status !== 'ACTIVE') continue;
+        console.log(`Syncing ${bunqAccount.description}`);
         await syncTransactionsForAccount({ bunq, ynab, bunqAccount, ynabBudget: budget });
       }
 
